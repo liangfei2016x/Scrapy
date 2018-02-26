8f4ab7605825411d15ba02c27443c0640f2a0666
liangfei2016x <liangfei2016x@163.com>
2018-02-26 20:57:11 +0800

    分页处理

--- dataoke/dataoke/spiders/dataoke_spider.py
+++ dataoke/dataoke/spiders/dataoke_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 from dataoke.items import DataokeItem
 
 class DmozSpider(scrapy.Spider):
@@ -12,14 +13,18 @@ class DmozSpider(scrapy.Spider):
         for sel in response.xpath('//*[@class="goods-list clearfix"]/div/div'):
             item = DataokeItem()
             item["img_url"] = sel.xpath("div[1]/a/img/@src").extract_first()
-            item["describe"] = sel.xpath("div[2]/span/a/text()").extract_first()
+            item["describe"] = re.sub("[\n\t]", "", sel.xpath("div[2]/span/a/text()").extract_first())
             item["price"] = sel.xpath("div[2]/div[2]/div[1]/p/b/text()").extract_first()
             item["marketing_plan"] = sel.xpath("div[2]/div[2]/div[2]/p/text()").extract_first()
             item["price1"] = sel.xpath("div[2]/div[3]/div[1]/p/b/text()").extract_first()
             item["sales_volume"] = sel.xpath("div[2]/div[3]/span/b/text()").extract_first()
             yield item
+        # 分页数据 从第二页抓起
+        totalPage = response.xpath('/html/body/div[3]/div[2]/div[2]/div/div[2]/b[1]/text()').extract_first().replace('/', '')
         pageNum = response.xpath('//div[@class="quan_page_main"]/span[@class="cur"]/text()').extract_first()
         next_num = int(pageNum) + 1
-        if next_num < 377:
+        if next_num <= int(totalPage):
             new_url = 'http://www.dataoke.com/qlist/?page={}'.format(str(next_num))
             yield scrapy.Request(new_url, callback=self.parse, dont_filter=True)
+
+
